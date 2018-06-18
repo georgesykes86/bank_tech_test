ef967097f2544ef3ac2a7c5d05b16490432aad28
George Sykes <georgesykes86@gmail.com>
2018-06-18 13:26:05 +0100

    Adds withdraw method to Account

--- src/main/java/com/makersacademy/banktechtest/Account.java
+++ src/main/java/com/makersacademy/banktechtest/Account.java
@@ -13,4 +13,6 @@ public class Account {
   public void deposit(int amount) {
     this.balance += amount;
   }
+
+  public void withdraw(int amount) { this.balance -= amount; }
 }
--- src/test/java/com/makersacademy/banktechtest/AccountTest.java
+++ src/test/java/com/makersacademy/banktechtest/AccountTest.java
@@ -31,4 +31,18 @@ public class AccountTest {
     account.deposit(200);
     assertEquals(250, account.getBalance());
   }
+
+  @Test
+  public void accountMakesWithdrawal() {
+    account.deposit(100);
+    account.withdraw(50);
+    assertEquals(50, account.getBalance());
+  }
+
+  @Test
+  public void accountMakesDifferentWithdrawal() {
+    account.deposit(100);
+    account.withdraw(100);
+    assertEquals(0, account.getBalance());
+  }
 }
