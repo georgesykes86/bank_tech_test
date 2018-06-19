b9544b7304df5f2e78756d723a387408a632999c
George Sykes <georgesykes86@gmail.com>
2018-06-19 06:45:12 +0100

    Adds the TransactionFactoryTest
    This was done as the TransactionRepository test was accidentally committed on previous commit. Please bear in mind that the TransactionRepositoryTest is currently not complete

--- src/test/java/com/makersacademy/banktechtest/Unit/TransactionFactoryTest.java
+++ src/test/java/com/makersacademy/banktechtest/Unit/TransactionFactoryTest.java
@@ -1,5 +1,20 @@
 package com.makersacademy.banktechtest.Unit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.makersacademy.banktechtest.Transaction;
+import com.makersacademy.banktechtest.TransactionFactory;
+import org.junit.jupiter.api.Test;
+
 public class TransactionFactoryTest {
+
+  private TransactionFactory factory = new TransactionFactory();
+
+  @Test
+  public void returnsANewFactory() {
+    Object transaction = TransactionFactory.getTransaction();
+    assertTrue(transaction instanceof Transaction);
+  }
 
 }
