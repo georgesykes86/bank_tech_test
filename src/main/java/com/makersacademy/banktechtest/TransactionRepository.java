f6740c6d53ed14fcb5a76d4fbb78b4ecdfb1719e
George Sykes <georgesykes86@gmail.com>
2018-06-19 06:41:28 +0100

    Updates Transaction Factory test

--- src/main/java/com/makersacademy/banktechtest/TransactionRepository.java
+++ src/main/java/com/makersacademy/banktechtest/TransactionRepository.java
@@ -1,21 +1,29 @@
 package com.makersacademy.banktechtest;
 
 import java.util.ArrayList;
+import javax.inject.Inject;
 
 public class TransactionRepository {
 
-  private ArrayList<Transaction> transactions;
+  private ArrayList<Object> transactions;
+  private Object transactionFactory;
 
   public TransactionRepository() {
-    this.transactions = new ArrayList<>();
+    this.transactions = new ArrayList();
+    this.transactionFactory = new TransactionFactory();
   }
 
-  public ArrayList<Transaction> getTransactions() {
+  public TransactionRepository(Object factory) {
+    this.transactions = new ArrayList();
+    this.transactionFactory = factory;
+  }
+
+  public ArrayList<Object> getTransactions() {
     return transactions;
   }
 
   public void addTransaction(int amount, Account account) {
-    transactions.add(new Transaction());
   }
+
 
 }
